7da0cb8f820fcba20045ff682024817195470ce1
Seelk89 <seelk.89@gmail.com>
2023-03-21 19:22:04 +0100

    minor refactor

--- Generator/GPTNeoGenerator.py
+++ Generator/GPTNeoGenerator.py
@@ -1,8 +1,8 @@
 import torch
-
-from Generator.generatorInterface import GeneratorInterface
 from pynvml import *
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+
+from Generator.generatorInterface import GeneratorInterface
 
 class GPTNeoGenerator(GeneratorInterface):
     def __init__(self):
@@ -10,31 +10,46 @@ class GPTNeoGenerator(GeneratorInterface):
         self.model = None
         self.use_cuda = False
 
+        # Check if a GPU is available
         if torch.cuda.is_available():
             nvmlInit()
             h = nvmlDeviceGetHandleByIndex(0)
             info = nvmlDeviceGetMemoryInfo(h)
             free_vram = info.free/1048576000
-            print(f'GPU with: {str(free_vram)} GB of free VRAM Detected.')
+            print(f'GPU with {free_vram:.2f} GB of free VRAM detected.')
 
-            if free_vram>13.5:
+            if free_vram > 13.5:
                 self.use_cuda = True
                 self.model_name = 'EleutherAI/gpt-neo-2.7B'
-                self.model = GPTNeoForCausalLM.from_pretrained(self.model_name)
-                self.model.to("cuda:0")
-            elif free_vram>13.5:
+            elif free_vram > 7:
                 self.use_cuda = True
-                self.model.to("cuda:0")
+                self.model.to('cuda:0')
 
+        # Load the model
         self.model = GPTNeoForCausalLM.from_pretrained(self.model_name)
         self.tokenizer = GPT2Tokenizer.from_pretrained(self.model_name)
 
-    def get_generated_text(self, prompt) -> str:
-        input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids
-        
+        # Move the model to the GPU if available
         if self.use_cuda:
-            input_ids = input_ids.cuda()
+            self.model.to('cuda:0')
 
-        gen_tokens = self.model.generate(input_ids, do_sample=True, temperature=0.9, max_length=100, pad_token_id=self.tokenizer.eos_token_id)
+    def get_generated_text(self, prompt) -> str:
+        try:
+            # Tokenize the input prompt
+            input_ids = self.tokenizer(prompt, return_tensors='pt', max_length=1024).input_ids
 
-        return self.tokenizer.batch_decode(gen_tokens)[0]
+            # Move the input to the GPU if available
+            if self.use_cuda:
+                input_ids = input_ids.cuda()
+
+            # Generate text
+            gen_tokens = self.model.generate(input_ids, do_sample=True, temperature=0.9, max_length=50, pad_token_id=self.tokenizer.eos_token_id)
+
+            # Decode the generated text
+            generated_text = self.tokenizer.batch_decode(gen_tokens)[0].strip()
+
+            return generated_text
+
+        except Exception as e:
+            print(f'Error generating text: {str(e)}')
+            return ''
--- conversation_manager.py
+++ conversation_manager.py
@@ -1,6 +1,6 @@
 import keyboard
-import multiprocessing
 
+# Import modules from the same package
 from Generator.generatorInterface import GeneratorInterface
 from Generator.GPTNeoGenerator import GPTNeoGenerator
 from Generator.GPTChatAPI import OpenaiApiDavinci, OpenaiApiGPT3Turbo
@@ -10,36 +10,59 @@ from TextToSpeech.pyttsx3TTS import Pyttsx3TTS
 from SpeechToText.STTInterface import STTInterface
 from SpeechToText.GoogleApi import GoogleAPISTT
 
-class Main:
+
+class ConversationManager:
+    # Manages a conversation between a user and an AI
+
     def __init__(self, generator: GeneratorInterface, tts: TTSInterface, stt: STTInterface = None):
+        # :param generator: an object that generates AI responses
+        # :param tts: an object that generates TTS audio
+        # :param stt: an object that transcribes speech to text
+
         self.generator = generator
         self.tts = tts
         self.stt = stt
 
-    def main(self):
+    def start_conversation(self):
+        # Starts a conversation between a user and an AI
         print('Starting loop.')
         while True:
             # Reset the prompt
             prompt = None
 
-            if self.stt == None:
+            if self.stt is None:
                 # For handling input from the terminal, as opposed to STT
                 prompt = str(input('User: '))
 
             # End the conversation if the user presses 'q'
-            if self.stt == None and prompt.lower() == 'q':
+            if self.stt is None and prompt.lower() == 'q':
                 break
 
             if keyboard.is_pressed('ctrl'):
                 prompt = self.stt.get_stt_text()
-            
-            if prompt != None:
+
+            if prompt is not None:
                 generated_text = self.generator.get_generated_text(prompt)
                 print(f'Ai: {generated_text}')
-                p = multiprocessing.Process(target=self.tts.get_tts_audio, args=(generated_text,))
-                p.start()
+
+                # Generate TTS audio and play it
+                self.play_tts_audio(generated_text)
+
+    def play_tts_audio(self, text: str):
+        # Generates TTS audio and plays it
+        try:
+            # Generate TTS audio
+            audio = self.tts.get_tts_audio(text)
+
+            # Play TTS audio
+            audio.play()
+        except Exception as e:
+            # Handle TTS audio generation or playback errors
+            print(f'Error playing audio: {str(e)}')
+
 
 if __name__ == '__main__':
+    # Initialize objects for the AI generator, TTS engine, and STT engine
     #generator = GPTNeoGenerator()
     #generator = OpenaiApiDavinci()
     generator = OpenaiApiGPT3Turbo()
@@ -47,5 +70,6 @@ if __name__ == '__main__':
     tts = Pyttsx3TTS()
     stt = GoogleAPISTT()
 
-    main = Main(generator, tts, stt)
-    main.main()
+    # Create a conversation manager and start the conversation
+    conversation_manager = ConversationManager(generator, tts, stt)
+    conversation_manager.start_conversation()
